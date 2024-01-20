4f7271f2c62b7fab6fdfa3e3494eeeb93c2ca2a4
Saranya <jsaran.bj@gmail.com>
2024-01-20 18:07:32 +0100

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
-console.log("Connected")
+
+// Wait for the DOM to finish lading before running the game
+//Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded",function(){
+    let button=document.getElementsByTagName("button");
+    for(button of buttons){
+        buttons.addEventListener("click",function(){
+          if(this.getAttribute("data-type")==="submit"){
+            alert("You clicked Submit");
+          }else{
+            let gameType=this.getAttributr("data-type");
+            alert(`You clicked ${data-type}`);
+          }  
+        })
+    }
+})
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+
+}
